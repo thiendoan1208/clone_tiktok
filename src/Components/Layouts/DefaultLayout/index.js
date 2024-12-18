fb2ea615f2ae73a17f6d33cdb8230a86fd3870ea
thiendoan1208 <thienmacro1234@gmail.com>
2024-12-17 20:27:10 -0500

    Layout Skeleton

--- src/Components/Layouts/DefaultLayout/index.js
+++ src/Components/Layouts/DefaultLayout/index.js
@@ -1,14 +1,22 @@
+import classNames from 'classnames/bind';
+
 import Header from '~/Components/Layouts/components/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+
+const cx = classNames.bind(styles)
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      
-      <div className="container">
+      <div className={cx('container')}>
+       <div className={cx('inner__con')}>
         <Sidebar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>
+          {children}
+        </div>
+       </div>
       </div>
     </div>
   );
--- src/Components/Layouts/components/Header/index.js
+++ src/Components/Layouts/components/Header/index.js
@@ -1,6 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss';
+
+// cho phép sử dụng - trong js
+const cx = classNames.bind(styles);
 
 function Header() {
-  return <h1>Header</h1>;
+  return <header className={cx('wrapper')}>
+    <div className={cx('inner__wrapper')}></div>
+  </header>;
 }
 
 export default Header;
