8804a24cad98e097a3b8d6db6ec7a13fddaab5b5
thiendoan1208 <thienmacro1234@gmail.com>
2024-12-27 21:00:59 -0500

    Watch Video Logic #1

--- src/Pages/Home/Videos/index.js
+++ src/Pages/Home/Videos/index.js
@@ -1,57 +1,59 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
-import styles from './Videos.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBookmark, faCommentDots, faHeart, faPlus, faShare } from '@fortawesome/free-solid-svg-icons';
 
+import styles from './Videos.module.scss';
 
 const cx = classNames.bind(styles);
 
-function Video() {
+function Video({ data }) {
   return (
     <div className={cx('wraper')}>
-      <video
-        className={cx('video')}
-        controls
-        src="https://files.fullstack.edu.vn/f8-tiktok/videos/520-63516c43aeede.mp4"
-      />
-     <div className={cx('status')}>
-          <div className={cx('avatar')}>
-            <img className={cx('avatar__user')} src="https://files.fullstack.edu.vn/f8-tiktok/users/10/630267063d1b4.jpg" alt="" />
-            <div className={cx('follow__btn')}>
-              <FontAwesomeIcon icon={faPlus} />
-            </div>
-          </div>
-    
-          <div className={cx('react')}>
-            <div className={cx('react__btn')}>
-              <FontAwesomeIcon icon={faHeart} />
-            </div>
-              <span className={cx('number')}>194.2K</span>
-          </div>
-    
-          <div className={cx('react')}>
-            <div className={cx('react__btn')}>
-              <FontAwesomeIcon icon={faCommentDots} />
-            </div>
-              <span className={cx('number')}>194.2K</span>
-          </div>
-    
-          <div className={cx('react')}>
-            <div className={cx('react__btn')}>
-              <FontAwesomeIcon icon={faBookmark} />
-            </div>
-              <span className={cx('number')}>194.2K</span>
-          </div>
-    
-          <div className={cx('react')}>
-            <div className={cx('react__btn')}>
-              <FontAwesomeIcon icon={faShare} />
-            </div>
-              <span className={cx('number')}>194.2K</span>
+      <video className={cx('video')}  preload="none" controls muted loop src={data.popular_video.file_url} />
+
+      <div className={cx('status')}>
+        <div className={cx('avatar')}>
+          <img className={cx('avatar__user')} src={data.avatar} alt="" />
+          <div className={cx('follow__btn')}>
+            <FontAwesomeIcon icon={faPlus} />
           </div>
         </div>
+
+        <div className={cx('react')}>
+          <div className={cx('react__btn')}>
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
+          <span className={cx('number')}>194.2K</span>
+        </div>
+
+        <div className={cx('react')}>
+          <div className={cx('react__btn')}>
+            <FontAwesomeIcon icon={faCommentDots} />
+          </div>
+          <span className={cx('number')}>194.2K</span>
+        </div>
+
+        <div className={cx('react')}>
+          <div className={cx('react__btn')}>
+            <FontAwesomeIcon icon={faBookmark} />
+          </div>
+          <span className={cx('number')}>194.2K</span>
+        </div>
+
+        <div className={cx('react')}>
+          <div className={cx('react__btn')}>
+            <FontAwesomeIcon icon={faShare} />
+          </div>
+          <span className={cx('number')}>194.2K</span>
+        </div>
+      </div>
     </div>
   );
 }
+
+Video.propTypes = {
+  data: PropTypes.node.isRequired,
+};
 
 export default Video;
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,30 +1,134 @@
 import classNames from 'classnames/bind';
-import styles from './Home.module.scss';
-import Video from './Videos';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+import Video from './Videos';
+import styles from './Home.module.scss';
+import * as request from '~/utils/httpsrequest';
+import { useEffect, useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function Home() {
+  const [videos, setVideo] = useState([]);
+  const [pages, setPage] = useState(1);
+  const [currentVideo, setCurrentVideo] = useState('');
+  const [isDisable, setIsDisable] = useState(true);
 
-  
+  const videoRefs = useRef([]);
+  const upBtn = useRef();
+
+  const videoArray = videoRefs.current;
+  const videoList = videoArray.length;
+
+  console.log(currentVideo);
+
+  // Call API
+  useEffect(() => {
+    request
+      .get('users/suggested?', {
+        params: {
+          page: pages,
+          per_page: 5,
+        },
+      })
+      .then((res) => {
+        setVideo((pre) => [...pre, ...res.data]);
+      });
+  }, [pages]);
+
+  // Observer
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const wrapper = entry.target;
+          const video = wrapper.querySelector('video');
+
+          if (entry.isIntersecting && video) {
+            video.muted = true;
+            video.play().catch((err) => {
+              console.warn('Video đang bị lỗi, F5 lại trang web để sửa', err);
+            });
+            setCurrentVideo(videoRefs.current.indexOf(entry.target));
+          } else if (!video && !entry.isIntersecting) {
+            video.pause();
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      },
+    );
+
+    videoRefs.current.forEach((video) => {
+      if (video) observer.observe(video);
+    });
+
+    return () => observer.disconnect();
+  }, [videos]);
+
+  // ScrollByHand + ScrollBtn
+  useEffect(() => {
+    if (currentVideo === videoList - 1) {
+      setPage((pre) => pre + 1);
+    }
+
+    if (currentVideo > 0) {
+      setIsDisable(false);
+    } else {
+      setIsDisable(true);
+    }
+  }, [currentVideo, videoList]);
+
+  // ClickScrollDown
+  const clickScrollDown = () => {
+    setTimeout(() => {
+      if (currentVideo <= videoList) {
+        videoArray[currentVideo + 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
+    }, 500);
+  };
+
+  // ClickClickScrollUp
+  const clickScrollUp = () => {
+    if (currentVideo > 0) {
+      videoArray[currentVideo - 1].scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+  };
+
   return (
-    <div className={cx('wrapper')}>
-      <div className={cx('video')}>
-        <Video />
+    <>
+      <div className={cx('wrapper')}>
+        {videos.map((item, index) => (
+          <div ref={(el) => (videoRefs.current[index] = el)} key={index} className={cx('video')}>
+            <Video data={item} />
+          </div>
+        ))}
       </div>
+
       <div className={cx('controls')}>
-        <button className={cx('control__button')}>
+        <button disabled={isDisable} ref={upBtn} className={cx('control__button')} onClick={clickScrollUp}>
           <FontAwesomeIcon icon={faChevronUp} />
         </button>
 
-        <button className={cx('control__button')}>
+        <button className={cx('control__button')} onClick={clickScrollDown}>
           <FontAwesomeIcon icon={faChevronDown} />
         </button>
       </div>
-    </div>
+    </>
   );
 }
 
 export default Home;
+
+// el cho phép bạn tham chiếu trực tiếp đến phần tử DOM.
+// videoRefs.current[index] cho phép bạn lưu trữ tham chiếu của từng phần tử video
+// trong mảng videoRefs.current theo chỉ mục của nó, giúp bạn truy cập và thao tác với từng video dễ dàng.
+
+// const index = videoRefs.current.indexOf(entry.target);
+//  -> VideoRef.current: tham chiếu đến mảng chứa tất cả các video
+//  -> entry.target: Lấy ra target của thẻ trong viewport
+//  -> indexOf(): kiểm tra entry.target là index thứ mấy của VideoRef.curents
+
+//   },[currentVideo, videoList]); điều kiện bên trong chỉ được gọi khi 1 trong 2 giá trị thay đổiđổi
