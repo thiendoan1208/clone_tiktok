f5c5c7149c796da3911e4251657756d5dca0cd45
thiendoan1208 <thienmacro1234@gmail.com>
2025-01-06 15:33:47 -0500

    Profile Logic #1

--- src/Pages/Profile/ProfileInfo/index.js
+++ src/Pages/Profile/ProfileInfo/index.js
@@ -1,32 +1,152 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGear, faPenToSquare, faShare, faX } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faGear, faPenToSquare, faShare, faX } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import images from '~/assets/images';
 
 import styles from './ProfileInfo.module.scss';
 import Buttons from '~/Components/Buttons';
 import ProfileInfoFollow from './ProfileInfoFollow';
+import { useEffect, useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function ProfileInfo() {
+  const [editProfile, setEditProfile] = useState(false);
+  const [preImg, setPreImg] = useState(images.userAvatar);
+  const [originalImg, setOriginalImg] = useState(preImg);
+  const [usernameInput, setUsernameInput] = useState('doan.thien.bhhh');
+  const [originalUserName, setOriginalUserName] = useState(usernameInput);
+  const [usernameCounter, setUsernameCounter] = useState(usernameInput.length);
+
+  const overlayRef = useRef();
+  const preImgInputRef = useRef();
+  const preImgRef = useRef();
+  const usernameInputRef = useRef();
+  const usernameErrorNotiRef = useRef();
+  const correctIconRef = useRef();
+  const usernameMaxErrorNotiRef = useRef();
+
+  const saveBtnRef = useRef();
+
+  // handle open edit profile menu
+  const handleEditProfile = () => {
+    if (editProfile === true) {
+      setEditProfile(false);
+      overlayRef.current.classList.remove(cx('active'));
+      // Profile Img
+      setPreImg(originalImg);
+      // Username change
+      setUsernameInput(originalUserName);
+    } else {
+      setEditProfile(true);
+      overlayRef.current.classList.add(cx('active'));
+    }
+  };
+
+  // handle preWatch image
+  const handlepreImg = () => {
+    const file = preImgInputRef.current.files[0];
+
+    if (file) {
+      let preImgSrc = preImgRef.current.src;
+      preImgSrc = URL.createObjectURL(file);
+      setPreImg(preImgSrc);
+    }
+  };
+
+  // handle UserName change
+  const handleValidateUsername = (e) => {
+    let value = e.target.value.trimStart();
+    const hasSpace = value.includes(' ');
+    setUsernameInput(value);
+
+    if (/[a-z]/.test(value) || /[A-Z]/.test(value) || /[0-9]/.test(value)) {
+      correctIconRef.current.classList.add(cx('correct'));
+      usernameInputRef.current.classList.remove(cx('error'));
+      usernameErrorNotiRef.current.classList.remove(cx('error'));
+    } else {
+      correctIconRef.current.classList.remove(cx('correct'));
+      usernameInputRef.current.classList.add(cx('error'));
+      usernameErrorNotiRef.current.classList.add(cx('error'));
+    }
+
+    if ((value.length === 0) & (value.length <= 24)) {
+      correctIconRef.current.classList.remove(cx('correct'));
+      usernameInputRef.current.classList.add(cx('error'));
+      usernameErrorNotiRef.current.classList.add(cx('error'));
+      usernameMaxErrorNotiRef.current.classList.remove(cx('error'));
+    } else if (value.length > 24) {
+      correctIconRef.current.classList.remove(cx('correct'));
+      usernameInputRef.current.classList.add(cx('error'));
+      usernameErrorNotiRef.current.classList.remove(cx('error'));
+      usernameMaxErrorNotiRef.current.classList.add(cx('error'));
+    } else {
+      correctIconRef.current.classList.add(cx('correct'));
+      usernameInputRef.current.classList.remove(cx('error'));
+      usernameErrorNotiRef.current.classList.remove(cx('error'));
+    }
+
+    if (hasSpace || value.length === 0) {
+      correctIconRef.current.classList.remove(cx('correct'));
+      usernameInputRef.current.classList.add(cx('error'));
+      usernameErrorNotiRef.current.classList.add(cx('error'));
+    } else {
+      correctIconRef.current.classList.add(cx('correct'));
+      usernameInputRef.current.classList.remove(cx('error'));
+      usernameErrorNotiRef.current.classList.remove(cx('error'));
+    }
+  };
+  // handle Name change
+
+  // handle Bio change
+
+  // hanle Save changes
+
+  useEffect(() => {
+    if (preImg === originalImg) {
+      saveBtnRef.current.classList.remove(cx('active'));
+    } else {
+      saveBtnRef.current.classList.add(cx('active'));
+    }
+  }, [originalImg, preImg, usernameCounter, usernameInput.length]);
+
+  useEffect(() => {
+    if (usernameCounter === usernameInput.length || usernameErrorNotiRef.current.classList.contains(cx('error'))) {
+      saveBtnRef.current.classList.remove(cx('active'));
+    } else {
+      saveBtnRef.current.classList.add(cx('active'));
+    }
+  }, [usernameCounter, usernameInput.length]);
+
+  const saveChange = () => {
+    // Overlay
+    setEditProfile(false);
+    // Profile Img
+    setOriginalImg(preImg);
+    // Username change
+    setOriginalUserName(usernameInput);
+    setUsernameCounter(usernameInput.length);
+
+    overlayRef.current.classList.remove(cx('active'));
+  };
+
   return (
     <div className={cx('wrapper')}>
       {/* Avatar */}
       <div className={cx('img')}>
-        <img src={images.userAvatar} alt="" />
+        <img src={preImg} alt="" />
       </div>
 
       {/* Information */}
       <div className={cx('infor')}>
         <div className={cx('nickname')}>
-          <h1>doan.thien.bhhh</h1>
+          <h1>{usernameInput}</h1>
           <h2>Doan Thien</h2>
         </div>
 
         {/* Controls */}
         <div className={cx('controls')}>
-          <Buttons primary className={cx('primary__btn')}>
+          <Buttons onClick={handleEditProfile} primary className={cx('primary__btn')}>
             Edit Profile
           </Buttons>
 
@@ -51,12 +171,12 @@ function ProfileInfo() {
       </div>
 
       {/* Edit Profile Overlay */}
-      <div className={cx('overlay')} tabIndex={-1}>
+      <div ref={overlayRef} className={cx('overlay')} tabIndex={-1}>
         <div className={cx('edit__profile')}>
           {/* Header */}
           <div className={cx('header')}>
             <h1 className={cx('header__text')}>Edit Profile</h1>
-            <FontAwesomeIcon className={cx('header_X')} icon={faX} />
+            <FontAwesomeIcon onClick={handleEditProfile} className={cx('header_X')} icon={faX} />
           </div>
           {/* Main */}
           <div className={cx('main')}>
@@ -67,10 +187,11 @@ function ProfileInfo() {
               </div>
               <div className={cx('photo__section')}>
                 <div className={cx('pre__photo')}>
-                  <img src="https://i.pinimg.com/736x/cd/c9/8b/cdc98beb5e2ec6518b5fd3852372351b.jpg" alt="" />
-                  <div className={cx('edit__img')}>
+                  <img ref={preImgRef} src={preImg} alt="" />
+                  <label htmlFor="imgPre" className={cx('edit__img')}>
                     <FontAwesomeIcon icon={faPenToSquare} />
-                  </div>
+                  </label>
+                  <input ref={preImgInputRef} onChange={handlepreImg} type="file" hidden id="imgPre" />
                 </div>
               </div>
             </div>
@@ -82,9 +203,27 @@ function ProfileInfo() {
               </div>
               <div className={cx('username__section')}>
                 <div className={cx('username__change')}>
-                  <input className={cx('username__input')} type="text" placeholder="Username" />
+                  <input
+                    onChange={handleValidateUsername}
+                    ref={usernameInputRef}
+                    className={cx('username__input')}
+                    type="text"
+                    placeholder="Username"
+                    value={usernameInput}
+                  />
+                  {/* Error noti when there is no username */}
+                  <span ref={usernameErrorNotiRef} className={cx('error__no-username')}>
+                    This username isn’t available. Please enter a new one.
+                  </span>
+                  {/* Error when > 24 */}
+                  <span ref={usernameMaxErrorNotiRef} className={cx('error__max-username')}>
+                    Maximum 24 characters.
+                  </span>
+                  {/* Correct when user enter username */}
+                  <FontAwesomeIcon ref={correctIconRef} className={cx('username__check--icon')} icon={faCheck} />
+
                   <div className={cx('username__rule')}>
-                    <span className={cx('username__link')}>www.tiktok.com/@doan.thien.bhhh</span>
+                    <span className={cx('username__link')}>{`www.tiktok.com/@${usernameInput}`}</span>
                     <span className={cx('rule')}>
                       Usernames can only contain letters, numbers, underscores, and periods. Changing your username will
                       also change your profile link.
@@ -101,9 +240,11 @@ function ProfileInfo() {
               </div>
               <div className={cx('name__section')}>
                 <div className={cx('name__change')}>
-                  <input className={cx('name__input')} type="text" placeholder="Name" />
+                  <input className={cx('name__input')} type="text" placeholder="Name(Incomplete)" />
                   <div className={cx('name__rule')}>
                     <span className={cx('rule')}>Your nickname can only be changed once every 7 days.</span>
+                    {/* error when name > 30 */}
+                    {/* <span className={cx('error__name')}>Maximum 30 characters</span>  */}
                   </div>
                 </div>
               </div>
@@ -116,7 +257,7 @@ function ProfileInfo() {
               </div>
               <div className={cx('bio__section')}>
                 <div className={cx('bio__change')}>
-                  <textarea className={cx('bio__input')} type="text" placeholder="Bio" />
+                  <textarea className={cx('bio__input')} type="text" placeholder="Bio(Incomplete)" />
                   <div className={cx('bio__rule', 'text__counter')}>
                     <span className={cx('rule')}>
                       <span className={cx('counter')}>0</span>/80
@@ -125,17 +266,16 @@ function ProfileInfo() {
                 </div>
               </div>
             </div>
-
-            {/* Save Area */}
-            <div className={cx('save__area')}>
-              <div className={cx('button__choice')}>
-                <Buttons outline className={cx('cancel')}>
-                  Cancel
-                </Buttons>
-                <Buttons outline className={cx('save')}>
-                  Save
-                </Buttons>
-              </div>
+          </div>
+          {/* Save Area */}
+          <div className={cx('save__area')}>
+            <div className={cx('button__choice')}>
+              <Buttons onClick={handleEditProfile} outline className={cx('cancel')}>
+                Cancel
+              </Buttons>
+              <Buttons ref={saveBtnRef} onClick={saveChange} outline className={cx('save')}>
+                Save
+              </Buttons>
             </div>
           </div>
         </div>
--- src/Pages/Profile/ProfileVideo/index.js
+++ src/Pages/Profile/ProfileVideo/index.js
@@ -2,16 +2,73 @@ import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faRetweet, faVideo, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faBookmark, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from './ProfileVideo.module.scss';
 
 const cx = classNames.bind(styles);
 
 function ProfileVideo() {
+  const [underlineWidth, setUnderlineWidth] = useState(138);
+  const [underLineLeft, setUnderlineLeft] = useState(0);
+
+  const navBarRef = useRef();
+  const categoryRef = useRef();
+  const underLineRef = useRef();
+
+  useEffect(() => {
+    const navBarItem = navBarRef.current.querySelectorAll('div');
+    navBarItem.forEach((element, index) => {
+      element.addEventListener('click', () => {
+        navBarItem.forEach((item) => {
+          item.classList.remove(cx('active'));
+        });
+        navBarItem[index].classList.add(cx('active'));
+        setUnderlineWidth(element.offsetWidth);
+        setUnderlineLeft(element.offsetLeft);
+      });
+    });
+  });
+
+  useEffect(() => {
+    const navBarItem = navBarRef.current.querySelectorAll('div');
+    navBarItem.forEach((element) => {
+      element.addEventListener('mouseover', () => {
+        setUnderlineLeft(element.offsetLeft);
+        setUnderlineWidth(element.offsetWidth)
+      });
+    });
+  });
+
+  useEffect(() => {
+    const activeItem = navBarRef.current.querySelector(`.${cx('active')}`);
+    // Viết class cx như vậy để khi trả về kết quả sẽ là .active thì mới querySelector được
+    // Nếu không có sẽ trả về active thì không thể queryselector được tới
+    const navBarItem = navBarRef.current.querySelectorAll('div');
+    navBarItem.forEach((element) => {
+      element.addEventListener('mouseleave', () => {
+        setUnderlineLeft(activeItem.offsetLeft);
+        setUnderlineWidth(activeItem.offsetWidth)
+      });
+    });
+  });
+
+  useEffect(() => {
+    const category = categoryRef.current.querySelectorAll('div');
+    category.forEach((element, index) => {
+      element.addEventListener('click', () => {
+        category.forEach((item) => {
+          item.classList.remove(cx('chose'));
+        });
+        category[index].classList.add(cx('chose'));
+      });
+    });
+  });
+
   return (
     <div className={cx('wrapper')}>
       <div className={cx('header')}>
-        <div className={cx('navbar')}>
+        <div ref={navBarRef} className={cx('navbar')}>
           <div className={cx('nav__item', 'active')}>
             <FontAwesomeIcon className={cx('icon')} icon={faVideo} />
             <span className={cx('section')}>Video</span>
@@ -31,10 +88,18 @@ function ProfileVideo() {
             <FontAwesomeIcon className={cx('icon')} icon={faHeart} />
             <span className={cx('section')}>Liked</span>
           </div>
+
+          <div className={cx('line__nav')}>
+            <div
+              style={{ width: `${underlineWidth}px`, left: `${underLineLeft}px` }}
+              ref={underLineRef}
+              className={cx('underline__nav')}
+            ></div>
+          </div>
         </div>
 
         <div className={cx('category__wrapper')}>
-          <div className={cx('category')}>
+          <div ref={categoryRef} className={cx('category')}>
             <div className={cx('cate__item', 'chose')}>
               <span className="item__name">Latest</span>
             </div>
@@ -49,14 +114,15 @@ function ProfileVideo() {
           </div>
         </div>
       </div>
+      <div className={cx('line')}></div>
 
       <div className={cx('main')}>
         <div className={cx('main__content')}>
-            <div className={cx('nothing')}>
+          <div className={cx('nothing')}>
             <FontAwesomeIcon icon={faCheck} />
-            </div>
-            <span className={cx('nothing__header')}>Upload your first video</span>
-            <span className={cx('nothing__main')}>Your video will appear here</span>
+          </div>
+          <span className={cx('nothing__header')}>Upload your first video</span>
+          <span className={cx('nothing__main')}>Your video will appear here</span>
         </div>
       </div>
     </div>
